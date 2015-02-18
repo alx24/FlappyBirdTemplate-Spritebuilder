2b2dcf42d6a8cbbed0bc6388ec45e0fdf953f1d9
alex ferreira <abrex18@hotmail.com>
2015-02-18 17:34:15 -0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -191,23 +191,23 @@
         }
         
     }
-        
-        for (CCNode *cloud in _clouds) {
-            
-            cloud.position = ccp(cloud.position.x -
-                                 (character.physicsBody.velocity.x * delta), cloud.position.y);
-            
-            // if the left corner is one complete width off the screen,
-            // move it to the right
-            if (cloud.position.x <= (-1 * cloud.contentSize.width)) {
-                cloud.position = ccp(cloud.position.x +
-                                     2 * cloud.contentSize.width, cloud.position.y);
-            
-            
-            }
-        }
-    
-    
+//        
+//        for (CCNode *cloud in _clouds) {
+//            
+//            cloud.position = ccp(cloud.position.x -
+//                                 (character.physicsBody.velocity.x * delta), cloud.position.y);
+//            
+//            // if the left corner is one complete width off the screen,
+//            // move it to the right
+//            if (cloud.position.x <= (-1 * cloud.contentSize.width)) {
+//                cloud.position = ccp(cloud.position.x +
+//                                     2 * cloud.contentSize.width, cloud.position.y);
+//            
+//            
+//            }
+//        }
+//    
+//    
     
     NSMutableArray *offScreenObstacles = nil;
     
